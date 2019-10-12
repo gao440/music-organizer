b3946afda497a988efef7cb5d15eebc9cd600aed
Blake Johnson <blake.k.johnson.4@gmail.com>
2019-10-12 10:21:29 -0400

    setup index file for the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,20 @@
-console.log('Hello world!');
+require('@babel/polyfill');
+require('dotenv').config();
 
+// Connect to the database
+const CONNECT_STRING = `mongodb+srv://monkey:${process.env.DB_PASSWORD}@cluster0-hjmzm.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+const mongoose = require('mongoose');
+mongoose.connect(CONNECT_STRING, { useNewUrlParser: true });
+
+// Setup express server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+// Setup routers
+
+// Start server
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
+});
