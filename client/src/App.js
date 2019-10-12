bb11bd17266eec1b1ee45e863a64aeed0014600f
Blake Johnson <blake.k.johnson.4@gmail.com>
2019-10-12 12:40:41 -0400

    setup proxy for local dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [ playlists, setPlaylists ] = useState([]);
+  useEffect(() => {
+    fetch('/playlists/bonk').then(res => res.json()).then(data => {
+      setPlaylists(data.items);
+      console.log(data.items);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is a test
     </div>
   );
 }
